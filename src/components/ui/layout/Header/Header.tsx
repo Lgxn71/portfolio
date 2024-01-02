81d4baf0a885fd7caddf26e81e4eb548b9ba5dfe
mymistyflowerfield <muhanai1913@gmail.com>
2024-01-02 17:15:45 +0900

    resume updated

--- src/components/ui/layout/Header/Header.tsx
+++ src/components/ui/layout/Header/Header.tsx
@@ -1,11 +1,14 @@
 import Links from "./Links";
 
 import styles from "./Header.module.css";
+import Link from "next/link";
 const Header = () => (
   <header className={styles.header}>
     <nav>
       <ul className={styles.links}>
-        <li className="logo">Ali</li>
+        <Link href="/" className="logo">
+          Ali
+        </Link>
         <Links />
       </ul>
     </nav>
